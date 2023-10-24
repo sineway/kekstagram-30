239223daf63d80fad5c96cc487a98cbe896ea99d
Sineway <sineway@yandex.com>
2023-10-24 12:28:36 +0300

    Заполним миниатюры необходимыми данными

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,8 +1,15 @@
 const container = document.querySelector('.pictures');
 const template = document.querySelector('#picture');
 
-const createThumbnails = (picturesData) => picturesData.map(() => {
+const createThumbnails = (picturesData) => picturesData.map((properties) => {
+  const {url, description, likes, comments} = properties;
   const thumbnail = template.content.querySelector('.picture').cloneNode(true);
+
+  thumbnail.querySelector('.picture__img').src = url;
+  thumbnail.querySelector('.picture__img').alt = description;
+  thumbnail.querySelector('.picture__likes').textContent = likes;
+  thumbnail.querySelector('.picture__comments').textContent = comments.length;
+
   return thumbnail;
 });
 
