821ec935d6006ee62897bc107702171b8cb17aa7
Sineway <sineway@yandex.com>
2023-10-30 21:21:58 +0300

    Реализуем закрытие всплывающего окна при нажатии на кнопку отмены

--- js/gallery/popup.js
+++ js/gallery/popup.js
@@ -1,14 +1,26 @@
 import {renderComments} from './comments.js';
 
 const popup = document.querySelector('.big-picture');
+const cancelButton = document.querySelector('.big-picture__cancel');
+
+const showPopup = () => {
+  popup.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+};
+
+const hidePopup = () => {
+  popup.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+};
 
 const renderPopup = ({url, description, likes, comments}) => {
   popup.querySelector('.big-picture__img img').src = url;
   popup.querySelector('.social__caption').textContent = description;
   popup.querySelector('.likes-count').textContent = likes;
   renderComments(comments);
-  popup.classList.remove('hidden');
-  document.body.classList.add('modal-open');
+  showPopup();
 };
+
+cancelButton.addEventListener('click', () => hidePopup());
 
 export {renderPopup};
