603c7beab95bddff005de11e6fdedb31b10759c9
Sineway <sineway@yandex.com>
2023-10-30 21:04:22 +0300

    Временно спрячем кнопку загрузки комментариев

--- js/gallery/comments.js
+++ js/gallery/comments.js
@@ -2,6 +2,7 @@ const container = document.querySelector('.social__comments');
 const template = document.querySelector('.social__comment');
 const shownCounter = document.querySelector('.social__comment-shown-count');
 const totalCounter = document.querySelector('.social__comment-total-count');
+const loaderButton = document.querySelector('.social__comments-loader');
 
 const createComments = (commentsData) => commentsData.map((properties) => {
   const {avatar, message, name} = properties;
@@ -18,6 +19,7 @@ const renderComments = (commentsData) => {
   container.replaceChildren(...createComments(commentsData));
   shownCounter.textContent = commentsData.length;
   totalCounter.textContent = commentsData.length;
+  loaderButton.classList.add('hidden');
 };
 
 export {renderComments};
