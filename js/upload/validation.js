20735be8adf220a781a59b5e0bf139630f5487ca
Sineway <sineway@yandex.com>
2023-11-06 00:32:46 +0300

    Реализуем базовую проверку хэш-тегов

--- js/upload/validation.js
+++ js/upload/validation.js
@@ -2,9 +2,15 @@ import '../../vendor/pristine/pristine.min.js';
 
 const form = document.querySelector('.img-upload__form');
 
-new Pristine(form, {
+const pristine = new Pristine(form, {
   classTo: 'img-upload__field-wrapper',
   errorTextParent: 'img-upload__field-wrapper',
   errorTextClass: 'img-upload__field-wrapper--error'
 });
 
+const split = (text) => text.split(' ').filter(Boolean);
+
+pristine.addValidator(form.hashtags, (text) => {
+  const hashtagPattern = /^#[a-zа-яё0-9]+$/i;
+  return split(text).every((word) => hashtagPattern.test(word));
+}, 'Невалидный хэш-тег', 1, true);
