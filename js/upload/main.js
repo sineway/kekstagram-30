4d5020d441d766eb3b3bf3554153e24d80ba24fc
Sineway <sineway@yandex.com>
2023-11-06 00:57:34 +0300

    Покажем окно редактирования при выборе файла

--- js/upload/main.js
+++ js/upload/main.js
@@ -1,4 +1,10 @@
 import {showPopup} from './popup.js';
 import './validation.js';
 
-showPopup();
+const form = document.querySelector('.img-upload__form');
+
+form.addEventListener('change', (event) => {
+  if (event.target.name === 'filename') {
+    showPopup();
+  }
+});
