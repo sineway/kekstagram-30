493db6c047295eb0cec709d6d034d685ea89bf70
Sineway <sineway@yandex.com>
2023-11-06 21:11:06 +0300

    Добавим настройки форматирования

--- js/upload/effects.js
+++ js/upload/effects.js
@@ -10,12 +10,22 @@ const ranges = {
   heat: [1, 3, .1]
 };
 
+const formatters = {
+  none: () => '',
+  chrome: (value) => `grayscale(${value})`,
+  sepia: (value) => `sepia(${value})`,
+  marvin: (value) => `invert(${value}%)`,
+  phobos: (value) => `blur(${value}px)`,
+  heat: (value) => `brightness(${value})`
+};
+
 const createOptions = (type) => {
   const [min, max, step] = ranges[type];
   return {
     range: {min, max},
     step,
     start: max,
+    format: {from: String, to: formatters[type]},
     connect: 'lower',
     tooltips: true
   };
