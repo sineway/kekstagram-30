53c266d43d9cdbc3cce286b0219394fc080485cf
Sineway <sineway@yandex.com>
2023-11-06 20:31:34 +0300

    Применим масштаб к изображению

--- js/upload/main.js
+++ js/upload/main.js
@@ -1,12 +1,18 @@
 import {showPopup} from './popup.js';
-import './scale.js';
+import {getScale} from './scale.js';
 import {checkValidity, resetValidity} from './validation.js';
 
 const form = document.querySelector('.img-upload__form');
+const preview = document.querySelector('.img-upload__preview img');
 
 form.addEventListener('change', (event) => {
-  if (event.target.name === 'filename') {
-    showPopup();
+  switch (event.target.name) {
+    case 'filename':
+      showPopup();
+      break;
+    case 'scale':
+      preview.style.transform = `scale(${getScale() / 100})`;
+      break;
   }
 });
 
--- js/upload/scale.js
+++ js/upload/scale.js
@@ -4,6 +4,7 @@ const setScale = (value) => {
   value = Math.max(value, 25);
   value = Math.min(value, 100);
   display.value = `${value}%`;
+  display.dispatchEvent(new Event('change', {bubbles: true}));
 };
 const getScale = () => Number.parseFloat(display.value);
 const scaleDown = () => setScale(getScale() - 25);
@@ -12,4 +13,4 @@ const scaleUp = () => setScale(getScale() + 25);
 scaleDownButton.addEventListener('click', () => scaleDown());
 scaleUpButton.addEventListener('click', () => scaleUp());
 
-export {scaleDown, scaleUp};
+export {getScale};
