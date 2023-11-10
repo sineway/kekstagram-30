ec1dc76768b32c6ae8afe4e2af78fb52ea246ae2
Sineway <sineway@yandex.com>
2023-11-10 10:50:20 +0300

    Удалим неиспользуемые функции

--- js/utilities.js
+++ js/utilities.js
@@ -7,15 +7,4 @@ const request = async (url, options) => {
   return response.json();
 };
 
-const getRandomItem = (items) => {
-  const fraction = items.length * Math.random();
-  const index = Math.floor(fraction);
-  return items.at(index);
-};
-
-const getRandomInteger = (min, max) => {
-  const fraction = (max - min) * Math.random() + min;
-  return Math.round(fraction);
-};
-
-export {request, getRandomItem, getRandomInteger};
+export {request};
