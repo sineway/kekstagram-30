e10825218cacc330530431f4a61cb3db1da88cdf
Sineway <sineway@yandex.com>
2023-11-10 09:58:27 +0300

    Реализуем закрытие сообщения при нажатии кнопки или за пределами окна

--- js/status.js
+++ js/status.js
@@ -2,6 +2,12 @@ const renderStatus = (type) => {
   const template = document.querySelector(`#${type}`);
   const status = template.content.querySelector(`.${type}`).cloneNode(true);
 
+  status.addEventListener('click', (event) => {
+    if (event.target.matches(`.${type}, .${type}__button`)) {
+      status.remove();
+    }
+  });
+
   document.body.append(status);
 };
 
