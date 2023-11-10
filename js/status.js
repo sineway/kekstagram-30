7703d35bbefa589b441b7d58ff5503334349d6f3
Sineway <sineway@yandex.com>
2023-11-10 10:05:25 +0300

    Реализуем закрытие сообщения при нажатии Esc

--- js/status.js
+++ js/status.js
@@ -2,13 +2,21 @@ const renderStatus = (type) => {
   const template = document.querySelector(`#${type}`);
   const status = template.content.querySelector(`.${type}`).cloneNode(true);
 
+  const onDocumentKeydown = (event) => {
+    if (event.key.startsWith('Esc')) {
+      status.click();
+    }
+  };
+
   status.addEventListener('click', (event) => {
     if (event.target.matches(`.${type}, .${type}__button`)) {
       status.remove();
+      document.removeEventListener('keydown', onDocumentKeydown);
     }
   });
 
   document.body.append(status);
+  document.addEventListener('keydown', onDocumentKeydown);
 };
 
 export {renderStatus};
