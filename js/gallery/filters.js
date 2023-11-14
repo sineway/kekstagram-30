2d9cd9bcc73c425fa2dbde1acd8ee43f132c262f
Sineway <sineway@yandex.com>
2023-11-14 10:28:53 +0300

    Добавим функцию selectFilter

--- js/gallery/filters.js
+++ js/gallery/filters.js
@@ -1,7 +1,13 @@
 const container = document.querySelector('.img-filters');
 
+const selectFilter = (id) => {
+  container.querySelectorAll('.img-filters__button').forEach((button) => {
+    button.classList.toggle('img-filters__button--active', button.id === id);
+  });
+};
+
 const renderFilters = () => {
   container.classList.remove('img-filters--inactive');
 };
 
-export {renderFilters};
+export {renderFilters, selectFilter};
